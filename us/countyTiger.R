4bd9d058ae6cdffbdc19b3ce909458aa80d1c8e9
Elon Cha <54544200+elonncha@users.noreply.github.com>
2020-08-10 16:46:43 -0400

    round area to 2 digits

--- us/countyTiger.R
+++ us/countyTiger.R
@@ -8,8 +8,8 @@ state = read_sf('tl_2019_us_state/tl_2019_us_state.shp')
 # drop geometry, select column, rename column names
 state_csv = st_drop_geometry(state) %>% 
   select(STATEFP, STUSPS, NAME, ALAND, INTPTLAT, INTPTLON) %>%
-  mutate(ALAND = ALAND / 1e+06) %>% # change unit from m2 to km2
-  rename(lat = INTPTLAT, lon = INTPTLON, name = NAME, landarea = ALAND, fips = STATEFP, state = STUSPS) %>%
+  mutate(ALAND = round(ALAND / 1e+06, 0)) %>% # change unit from m2 to km2, round to integer
+  rename(lat = INTPTLAT, lon = INTPTLON, name = NAME, kilometers = ALAND, fips = STATEFP, state = STUSPS) %>%
   mutate(lat = as.numeric(lat), lon = as.numeric(lon))
 
 
