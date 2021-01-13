788223724f5f833c8c0b0a9cb8dfd2a40a2a1e0d
kwinglee <kwinglee@users.noreply.github.com>
2021-01-13 18:15:17 -0500

    clean csv and add dropdown

--- us/countyTiger.R
+++ us/countyTiger.R
@@ -6,12 +6,28 @@ library(sf)
 state = read_sf('tl_2019_us_state/tl_2019_us_state.shp')
 
 # drop geometry, select column, rename column names
+num_decimals = 4
 state_csv = st_drop_geometry(state) %>% 
   select(STATEFP, STUSPS, NAME, ALAND, INTPTLAT, INTPTLON) %>%
   mutate(ALAND = round(ALAND / 1e+06, 0)) %>% # change unit from m2 to km2, round to integer
   rename(lat = INTPTLAT, lon = INTPTLON, name = NAME, kilometers = ALAND, fips = STATEFP, state = STUSPS) %>%
-  mutate(lat = as.numeric(lat), lon = as.numeric(lon))
+  mutate(lat = round(as.numeric(lat), num_decimals), lon = round(as.numeric(lon), num_decimals))
 
 
 # write csv
 readr::write_csv(state_csv, 'select.csv')
+
+# write dropdown menu list
+outfile = "dropdown_menu_list.html"
+cat("", file = outfile)
+for(row in 1:nrow(state_csv)) {
+  cat(paste0("<option value=\"", state_csv$state[row],
+             "\" stateid=\"", state_csv$fips[row],
+             "\" lat=\"", state_csv$lat[row],
+             "\" lon=\"", state_csv$lon[row],
+             "\" km=\"", state_csv$kilometers[row],
+             "\">", state_csv$name[row],
+             "</option>\n"),
+      file = outfile,
+      append = T)
+}
