b4c98a8d7c0cdef29a89d60f709487ff7c211027
benliu4 <96307509+benliu4@users.noreply.github.com>
2022-03-16 10:09:38 -0400

    'message'

--- process/naics/split_zip_data.py
+++ process/naics/split_zip_data.py
@@ -1,47 +1,77 @@
 # Zips
+# Retrieving NAICS data for each (zip code, IND level) pair
+# 2/23/2022
 
-#from uszipcode import SearchEngine
 import os
 import json, requests, csv
+import pandas as pd
 
-#base_url = "http://api.census.gov/data/2018/zbp?get=NAICS2017,ZIPCODE,ESTAB,EMPSZES&for=&INDLEVEL="
-
-url_beg = "api.census.gov/data/2018/zbp?get=NAICS2017,ESTAB,EMPSZES&for=ZIPCODE:"
-url_end = "&for=&INDLEVEL="
-
-inds = ["2", "3", "4", "5", "6"]
-raw_nums = list(range(0,99951))
+inds = ["2", "4", "6"]
+raw_nums = list(range(501,99951))
 zips = []
 
-for number in raw_nums:
+for number in raw_nums: # appending 0's to begining of zip codes with <5 numbers
     if len(str(number)) == 3:
         zips.append("00" + str(number))
     elif len(str(number)) == 4:
         zips.append("0" + str(number))
     else:
-        zips.append(number)
+        zips.append(str(number))
 
-def zipcode_data():
-    for i in zips:
-        for j in inds:
-            url = base_beg + str(i) + url_end + j
-            response = requests.get(url)
-            data = response.json()
+def create_folders(): # creates a folder for every zip code
+    for each in zips:
+        if not os.path.exists("zips/"+each): # check if directory for zipcode already there before
+            os.makedirs("zips/"+each)
+
+### Explanation of  zipcode() Function
+### In the zipcode() function, we have a base url which retrieves all relevant data for a certain IND Level. We can loop through each IND
+### level and therefore retrieve all data for whichever IND Levels we are interested in.
+
+### Following, we create a response object containing the data and use .json() to get the data into a json format, which we then write
+### to a file. Then we open the file, and create a pandas DF of all the data. To generate the zip code folders, we group the dataframe
+### by the "Zip Code" column and export it as a .csv file into the corresponding folder within naics/zips/xxxxx
+
+### The total time it took to run was around 2.5 hours
+
+def zipcode(): # populates zip code folders with data for each zip
+    for j in inds:
+        url = "https://api.census.gov/data/2018/zbp?get=ZIPCODE,NAICS2017,ESTAB,EMPSZES,PAYANN&for=&INDLEVEL=" + j
+        response = requests.get(url)
+        data = response.json()
+        with open('ind2_data.json', 'w') as f:
+            json.dump(data, f)
+
+        with open("ind2_data.json", "r") as f2:
+            jsdata = json.load(f2)
+
+        df = pd.DataFrame(jsdata, columns = ["Zip Code", "naics", "Establishments", "Employees", "Payroll", "IND Level"])
+
+        for num in zips[30000:30500]:
+            if df.loc[df["Zip Code"] == num].empty == True:
+                continue
+            elif not os.path.exists("zips/" + num): # check if directory for zipcode already there before
+                os.makedirs("zips/" + num)
+                df.loc[df["Zip Code"] == num].to_csv("zips/" + num + "/zipcode" + num + "-naics" + j + "-2018" + ".csv", index = False)
+            else:
+                df.loc[df["Zip Code"] == num].to_csv("zips/" + num + "/zipcode" + num + "-naics" + j + "-2018" + ".csv", index = False)
+        print("Done with IND")
+
+
+def delete_empty(): # deletes zip folders without data in them
+    dir = "zips"
+    folders = list(os.walk(dir))[1:]
+
+    for folder in folders:
+        if not folder[2]:
+            os.rmdir(folder[0])
 
 
 
 
-
-def zipcodes(indlevel):
-    url = base_url + str(indlevel)
-    response = requests.get(url)
-    data = response.json()
-
-    with open('feb11_test.csv', 'w', newline = "") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-
-### Function Call
+### Function Calls
 if __name__ == '__main__':
-    #zipcodes(6)
-    print("OK")
+    #create_folders()
+    zipcode()
+    #delete_empty()
+    print("Complete...")
+
