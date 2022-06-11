1ce080b2e48346b6d6803ade6e34b99a0096ca1c
benliu4 <96307509+benliu4@users.noreply.github.com>
2022-06-10 23:26:39 -0400

    Single Zipcode File Added
    Single Zipcode File Added. Will be updated to make more efficient

---
+++ single_zipcode.py
@@ -0,0 +1,57 @@
+# Python Script: Single Zipcode
+# This script allows the user to input a single zipcode (string), and it outputs the data for that zipcode
+# Ex: '98006', '30308', '00501'
+
+# Note this is an inefficient solution, will be updated 6/10/2022
+
+import os
+import json, requests, csv
+import pandas as pd
+
+
+### Explanation of  zipcode() Function
+### In the zipcode() function, we have a base url which retrieves all relevant data for a certain IND Level. We can loop through each IND
+### level and therefore retrieve all data for whichever IND Levels we are interested in.
+
+### Following, we create a response object containing the data and use .json() to get the data into a json format, which we then write
+### to a file. Then we open the file, and create a pandas DF of all the data. To generate the zip code folders, we group the dataframe
+### by the "Zip Code" column and export it as a .csv file into the corresponding folder within naics/zips/xxxxx
+
+inds = ["2"]
+
+def single_zipcode(input_zip): # populates zip code folders with data for each zip
+    for j in inds:
+        url = "https://api.census.gov/data/2018/zbp?get=ZIPCODE,NAICS2017,ESTAB,EMPSZES,PAYANN&for=&INDLEVEL=" + j
+        response = requests.get(url)
+        data = response.json()
+        with open('ind_data.json', 'w') as f:
+            json.dump(data, f)
+
+        with open("ind_data.json", "r") as f2:
+            jsdata = json.load(f2)
+
+        df = pd.DataFrame(jsdata, columns = ["Zip", "Naics", "Establishments", "Employees", "Payroll", "NaicsLevel"])
+        df['Employees'] =  df['Employees'].str.lstrip('0')
+
+
+        if df.loc[df["Zip"] == input_zip].empty == True:
+            continue
+        if not os.path.exists("us/zipcodes/naics/" + input_zip[0]): # check if directory for 1st num already there
+            os.makedirs("us/zipcodes/naics/" + input_zip[0])
+        if not os.path.exists("us/zipcodes/naics/" + input_zip[0] + "/" + input_zip[1]):
+            os.makedirs("us/zipcodes/naics/" + input_zip[0] + "/" + input_zip[1])
+        if not os.path.exists("us/zipcodes/naics/" + input_zip[0] + "/" + input_zip[1] + "/" + input_zip[2]):
+            os.makedirs("us/zipcodes/naics/" + input_zip[0] + "/" + input_zip[1] + "/" + input_zip[2])
+        if not os.path.exists("us/zipcodes/naics/" + input_zip[0] + "/" + input_zip[1] + "/" + input_zip[2] + "/" + input_zip[3]):
+            os.makedirs("us/zipcodes/naics/" + input_zip[0] + "/" + input_zip[1] + "/" + input_zip[2] + "/" + input_zip[3])
+        if not os.path.exists("us/zipcodes/naics/" + input_zip[0] + "/" + input_zip[1] + "/" + input_zip[2] + "/" + input_zip[3] + "/" + input_zip[4]):
+            os.makedirs("us/zipcodes/naics/" + input_zip[0] + "/" + input_zip[1] + "/" + input_zip[2] + "/" + input_zip[3] + "/" + input_zip[4])
+
+        df.loc[df["Zip"] == input_zip].to_csv("us/zipcodes/naics/" + input_zip[0] + "/" + input_zip[1] + "/" 
+        + input_zip[2] + "/" + input_zip[3] + "/" + input_zip[4] + "/zipcode" + input_zip + "-census-naics" + j + "-2018" + ".csv", index = False)
+
+        print("Done")
+
+## Function Call
+single_zipcode("98006")
+print("Run Complete")
